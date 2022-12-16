c8dd360a9aef19d8e387c220476946c444bd999d
Micutzuu25 <alexandrucristi25@yahoo.com>
2022-12-16 10:06:12 +0200

    Add project files.

---
+++ Data/Pop_Alexandru_Cristian_Lab2Context.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Pop_Alexandru_Cristian_Lab2.Models;
+
+namespace Pop_Alexandru_Cristian_Lab2.Data
+{
+    public class Pop_Alexandru_Cristian_Lab2Context : DbContext
+    {
+        public Pop_Alexandru_Cristian_Lab2Context (DbContextOptions<Pop_Alexandru_Cristian_Lab2Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Pop_Alexandru_Cristian_Lab2.Models.Book> Book { get; set; } = default!;
+
+        public DbSet<Pop_Alexandru_Cristian_Lab2.Models.Publisher> Publisher { get; set; }
+
+        public DbSet<Pop_Alexandru_Cristian_Lab2.Models.Category> Category { get; set; }
+    }
+}
---
+++ Pages/Categories/Index.cshtml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Pop_Alexandru_Cristian_Lab2.Data;
+using Pop_Alexandru_Cristian_Lab2.Models;
+
+namespace Pop_Alexandru_Cristian_Lab2.Pages.Categories
+{
+    public class IndexModel : PageModel
+    {
+        private readonly Pop_Alexandru_Cristian_Lab2.Data.Pop_Alexandru_Cristian_Lab2Context _context;
+
+        public IndexModel(Pop_Alexandru_Cristian_Lab2.Data.Pop_Alexandru_Cristian_Lab2Context context)
+        {
+            _context = context;
+        }
+
+        public IList<Category> Category { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            if (_context.Category != null)
+            {
+                Category = await _context.Category.ToListAsync();
+            }
+        }
+    }
+}
